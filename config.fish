6160897aceef12ed43a3bc60edebff22acb7c813
Yuichiro Kawano <tresener.yu1ro@gmail.com>
2019-04-11 12:35:08 +0900

    update

--- config.fish
+++ config.fish
@@ -1,5 +1,7 @@
 alias upgrade='brew upgrade;brew cleanup;fisher self-update;softwareupdate --install --all'
 alias ll='ls -al'
 alias gitp='git push'
+
 set PATH $HOME/.nodebrew/current/bin $PATH
+set -x PATH $HOME/.pyenv/bin $PATH
 
