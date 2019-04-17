a919a843f74cc5033c7f38caaf2ff083bc922961
yuichiro-kawano <tresener.yu1ro@gmail.com>
2019-04-17 16:00:02 +0900

    update push alias

--- config.fish
+++ config.fish
@@ -1,7 +1,10 @@
 alias upgrade='brew upgrade;brew cleanup;fisher self-update;softwareupdate --install --all'
 alias ll='ls -al'
-alias gitp='git push'
+alias gitp='git push -u'
+alias ls='exa'
 
-set PATH $HOME/.nodebrew/current/bin $PATH
+set PATH $HOME/.nodebrew/current/bin /Users/yuichiro-kawano/Library/Android/sdk/platform-tools $PATH
 set -x PATH $HOME/.pyenv/bin $PATH
 
+set -x PATH $HOME/.anyenv/bin $PATH
+eval (anyenv init - | source)
